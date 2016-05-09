916878673b14bae20e72daf0636f72aea506917c
mooshad1 <jakep@udel.edu>
2016-05-08 20:04:28 -0400

    Still working on sky

--- src/BackgroundHandler.java
+++ src/BackgroundHandler.java
@@ -18,7 +18,8 @@ public class BackgroundHandler extends Handler {
 			sand = ImageIO.read(new File("img/background/tile_sand_center.png"));//.getScaledInstance(1000, 1000, Image.SCALE_FAST);	
 			water = ImageIO.read(new File("img/background/river_revised.jpg"))
 					.getScaledInstance(Launcher.WIDTH, Launcher.HEIGHT / 10, Image.SCALE_FAST);
-			sky = ImageIO.read(new File("img/background/sky_tile.jpg"));
+			sky = ImageIO.read(new File("img/background/sky_tile.png"))
+					.getScaledInstance(Launcher.WIDTH / 10, Launcher.HEIGHT / 10, Image.SCALE_FAST);
 		}
 		catch(IOException e)
 		{
@@ -27,14 +28,14 @@ public class BackgroundHandler extends Handler {
 	}
 	
 	public void paint(Graphics g) {
-		for(int i = 0; i < Launcher.WIDTH; i+= sky.getWidth(null)){
-			g.drawImage(sky, i, 0, Color.gray, null);
-		}
-		
 		for(int i = 0; i < Launcher.WIDTH; i += sand.getWidth(null)){
 			for(int j = 0; j < Launcher.HEIGHT; j+= sand.getWidth(null)){
 				g.drawImage(sand, i, j, Color.gray, null);
 			}
+		}
+		
+		for(int i = 0; i < Launcher.WIDTH; i+= sky.getWidth(null)){
+			g.drawImage(sky, i, 0, Color.gray, null);
 		}
 		
 		g.drawImage(water, 0, Launcher.HEIGHT / 4, null);
