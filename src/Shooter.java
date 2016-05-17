c041387e6f778b87309306350f11dc8792c8779e
mooshad1 <jakep@udel.edu>
2016-05-17 17:35:03 -0400

    arrow y axis corrected

--- src/Shooter.java
+++ src/Shooter.java
@@ -12,7 +12,7 @@ public class Shooter {
 	public Shooter(GameState game){
 		arrow_or_pb_flag = true;
 		//game_arrow = new Arrow(GameState.frameWidth / 2 - 50, GameState.frameHeight - 330, 0, 0, game);
-		game_arrow = new Arrow(Launcher.WIDTH / 2 - ShooterHandler.arrowWidth / 2, Launcher.HEIGHT - 220, 0, 0, game);
+		game_arrow = new Arrow(Launcher.WIDTH / 2 - ShooterHandler.arrowWidth / 2, Launcher.HEIGHT - Launcher.HEIGHT / 4, 0, 0, game);
 		game_powerbar = new PowerBar(Launcher.WIDTH/5, Launcher.HEIGHT - 220, 0, 0, game);
 	}
 	/**
